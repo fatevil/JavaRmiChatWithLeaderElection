1809309ca2a52302e856a26230019ebf9067a28d
Marek Kozlovsky <marek.kozlovsky@jumpshot.com>
2020-01-12 12:41:19 +0100

    Lambda introduces.

--- src/main/java/cz/cvut/kozlovsky/run/Master.java
+++ src/main/java/cz/cvut/kozlovsky/run/Master.java
@@ -18,23 +18,21 @@ public class Master {
 
         NodeImpl nodeImpl = new NodeImpl(id, ipAddress, port, name);
 
-        Thread thread = new Thread() {
-            public void run() {
+        Thread thread = new Thread(() -> {
 
-                final int id = 101;
-                final String ipAddress = "127.0.0.1";
-                final int port = 2111;
-                final String name = "Bobek";
-                final String remoteAddress = "127.0.0.1";
-                final int remotePort = 2110;
+            final int id1 = 101;
+            final String ipAddress1 = "127.0.0.1";
+            final int port1 = 2111;
+            final String name1 = "Bobek";
+            final String remoteAddress = "127.0.0.1";
+            final int remotePort = 2110;
 
-                try {
-                    NodeImpl nodeImpl = new NodeImpl(id, ipAddress, port, name, remoteAddress, remotePort);
-                } catch (RemoteException | NotBoundException | MalformedURLException | AlreadyBoundException e) {
-                    e.printStackTrace();
-                }
+            try {
+                NodeImpl nodeImpl1 = new NodeImpl(id1, ipAddress1, port1, name1, remoteAddress, remotePort);
+            } catch (RemoteException | NotBoundException | MalformedURLException | AlreadyBoundException e) {
+                e.printStackTrace();
             }
-        };
+        });
 
 //        thread.start();
 
